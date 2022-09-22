ce1519d11fd99cd5c096f6890ea158b251a514e4
NawImNate <jnhatch3@gmail.com>
2022-09-22 13:50:14 -0400

    made form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import "./app.css";
+
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="title" variant="outlined" />
+        <TextField id="outlined-basic" label="description" variant="outlined" />
+        <Button type="submit" variant="outlined">
+          Submit
+        </Button>
+      </Box>
     </div>
   );
 }
