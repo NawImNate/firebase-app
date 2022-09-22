a0452d81198356ebd7bd9956efec3115ed402e62
NawImNate <jnhatch3@gmail.com>
2022-09-22 16:06:48 -0400

    map posts to render title & desc, create state

--- src/App.js
+++ src/App.js
@@ -2,8 +2,30 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
 import "./app.css";
+import { useEffect, useState } from "react";
+import { db } from "./firebase_config";
+import { collection, getDocs } from "firebase/firestore";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+// import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 
 function App() {
+  // state that will hold posts from our table/collection
+  const [posts, setPosts] = useState([]);
+  const postsRef = collection(db, "posts");
+
+  // useEffect to render list. when making a request with an api the api will try to return a promise(data that needs to be resolved, either a success or a failure), when making api calls use async await functions
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsRef);
+      setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  }, []);
+
   return (
     <div className="App">
       <Box
@@ -20,6 +42,30 @@ function App() {
           Submit
         </Button>
       </Box>
+      {posts.map((post) => {
+        return (
+          <Card sx={{ maxWidth: 345 }}>
+            {/* <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            /> */}
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                <h6>{post.title}</h6>
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                <p>{post.description}</p>
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        );
+      })}
     </div>
   );
 }
