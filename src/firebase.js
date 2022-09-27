54ed5a3c6a73018519c30f5f6aa72e47d0002a9c
NawImNate <jnhatch3@gmail.com>
2022-09-26 22:15:11 -0400

    init firebase storage for storing images

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "/firebase/firestore";
+import { getFirestore, getStorage } from "/firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDiVcUT4niSWqIXICouY82QgqhJ_WI43bk",
@@ -14,3 +14,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
+export const storage = getStorage(app);
